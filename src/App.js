a7f100c02df9c4f53cc81a43918dee72bb6d8c41
bhagyesh gahdage <81037937+BhagyeshGhadage@users.noreply.github.com>
2024-02-07 22:03:11 +0530

    [EDPS-285] Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. New changes are done with testing branch.
+          Edit <code>src/App.js</code> and save to reload. New changes are done with testing branch test.
         </p>
         <a
           className="App-link"
